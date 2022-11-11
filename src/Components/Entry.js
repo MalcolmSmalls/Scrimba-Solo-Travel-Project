eec2f916b676d4bab52e9246714523affaac36d0
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-11 12:24:58 -0800

    Finish static entry and CSS

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -16,6 +16,12 @@ function Entry () {
                     <a href="">View on Google Maps</a>
                 </span>
                 <h2>Mount Fuji</h2>
+                <span className = "date-span">
+                    12 Jan, 2021 - 24 Jan, 2021
+                </span>
+                <p className = "entry-paragraph">
+                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
             </div>
         </div>
     )
