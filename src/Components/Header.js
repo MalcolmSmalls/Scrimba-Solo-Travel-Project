f137bb6ec6eaa452193d570383fe61dbbf0aaf6c
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-11 11:43:50 -0800

    Finish header, add styling with CSS

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 export default function Header() {
     return (
         <header>
+            <i className="fa-solid fa-earth-americas"></i>
             <h1>My travel journal.</h1>
         </header>
     )
